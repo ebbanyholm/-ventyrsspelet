b8434dfda32db36e26b2ed4380ce91d589c04934
Ebba Nyholm <ebba.nyholm@skola.taby.se>
2022-01-12 20:29:09 +0100

    .

--- aventyr.py
+++ aventyr.py
@@ -6,7 +6,7 @@ from random import randint
 
 class Player():
     def __init__(self):
-        self.stark = 9
+        self.styrka = 9
         self.hp = 100                 #kopplar spelaren med dess styrka, lvl, hp och inventory
         self.lvl = 1
         self.inventory = []
@@ -14,22 +14,22 @@ class Player():
     def stats(self):
         print("")
         print(f"Du har {self.hp} hp kvar")
-        print(f"Din styrka är {self.stark}")
+        print(f"Din styrka är {self.styrka}")
         print(f"Din lvl är {self.lvl}")
         print("")
 
     def styrke_bonus(self):                 #ger spelaren styrke bonus beroende på inventoryt
-        self.stark = 9 + self.lvl
+        self.styrka = 9 + self.lvl
         if "svärd_4" in self.inventory:
-            self.stark += 4
+            self.styrka += 4
         if "pilbåge_2" in self.inventory:
-            self.stark += 2
+            self.styrka += 2
         if "kniv_3" in self.inventory:
-            self.stark += 3
+            self.styrka += 3
         if "sköld_2" in self.inventory:
-            self.stark += 2
+            self.styrka += 2
         if "yxa_3" in self.inventory:
-            self.stark += 3
+            self.styrka += 3
       
     def visa_inventory(self):
         if "svärd_4" in self.inventory:
@@ -52,13 +52,20 @@ class Player():
         
         if val_av_dörr in {"1","2","3"}:
 
-            dörrens_innehåll =randint(1,9)
+            dörrens_innehåll =randint(1,3)
 
             def inventory_fullt():
-                print("ditt inventory är fullt..")
-                print("vill du ersätta något?")
-                print("ja [w]")                              
-                print("nej [d]")    
+                print('''
+                Ditt inventory är fullt..")
+                Vill du ersätta något?
+
+                ja [w]                              
+                nej [d]
+                ''')  
+
+            def visa_inventory_kista():
+                print("FÖREMÅL_STYRKA")
+                print(self.inventory)
 
             def ersätta():
                 while True:
@@ -77,17 +84,13 @@ class Player():
                         print("felaktig input")
                         continue
             
-            def visa_inventory_kista():
-                print("FÖREMÅL_STYRKA")
-                print(self.inventory)
-                
             def redo_för_val():
              print("Du är nu redo för ditt nästa val.")
             
 
             
             # -------------------------------------- skattkista --------------------------------------#
-            if dörrens_innehåll in {1,2,3}:
+            if dörrens_innehåll == 1:
                 print("Yey, en skattkista!!")
 
                 while True:
@@ -244,22 +247,22 @@ class Player():
 
 
             # ------------------------------------------ fälla ---------------------------------------#                
-            if dörrens_innehåll in {4,5,6}:
+            if dörrens_innehåll == 2:
                 i = randint(1, 6)
                 
                 if i == 1:
                     print('''
-                    Du går in i en restaurangen. Ett bord flyger rätt emot dig. 
-                   
-                    Det blir svart....
+                        Du går in i en restaurangen. Ett bord flyger rätt emot dig. 
                     
-                    Du förlorade fyra hälsopoäng.
-                  
-                    Efter en stund vaknar du upp igen..
+                        Det blir svart....
+                        
+                        Du förlorade fyra hälsopoäng.
                     
-                    Resturangen är nu borta..
-                   
-                    ... tillbaka i korridoren ...
+                        Efter en stund vaknar du upp igen..
+                        
+                        Resturangen är nu borta..
+                    
+                        ... tillbaka i korridoren ...
                     
                     ''')
                     
@@ -268,20 +271,23 @@ class Player():
 
                 if i == 2:
                     antal_knivar = randint(1, 5)
-                    print(f'''Du gick på en fallucka.
-                    Under ditt fall stöter du på {antal_knivar} knivar
-                    
-                    Du förlorade {antal_knivar} hälsopoäng
-        
-                    Du lander sedan på en madrass och är redo för ditt nästa val
-                    ''')
+                    print(f'''
+                        Du gick på en fallucka.
+                        Under ditt fall stöter du på {antal_knivar} knivar.
+                        
+                        Du förlorade {antal_knivar} hälsopoäng.
+            
+                        Du lander sedan på en madrass och är redo för ditt nästa val.
+
+                        ''')
                     self.hp -= antal_knivar
                 
                 if i == 3:
-                    print('''Du gick in i en glas vägg och fick en lätt hjärnskakning 
-                    och bröt din näsa
-                    
-                    Du förlorade 2 hälsopoäng
+                    print('''
+                        Du gick in i en glas vägg och fick en lätt hjärnskakning 
+                        och bröt din näsa.
+                        
+                        Du förlorade 2 hälsopoäng.
                         
                     ''')
                     self.hp -= 2
@@ -289,42 +295,47 @@ class Player():
                     
 
                 if i == 4:
-                    print('''Du stöter på en tom dörr...
-           
-                    På din väg genom dörren stukar du din högra fot och förlorar 1 hälsopoäng.
-                    
+                    print('''
+                        Du stöter på en tom dörr...
+            
+                        På din väg genom dörren stukar du din högra fot och förlorar 1 hälsopoäng.
+                        
                     ''')
                     self.hp -= 1  
                     redo_för_val() 
 
                 if i == 5:
-                    print('''Du stöter på en tom dörr.
-                    
-                    På din väg genom dörren stukar du din vänstra fot och förlorar 1 hälsopoäng.
+                    print('''
+                        Du stöter på en tom dörr.
+                        
+                        På din väg genom dörren stukar du din vänstra fot och förlorar 1 hälsopoäng.
 
                     ''')
                     self.hp -= 1  
                     redo_för_val() 
 
                 if i == 6:
-                    print("Du sträcker ut din hand redo att öppna dörren framför dig.")
-                    print("När din hud kommer i kontakt med dörrhantaget börjar du SssKKaKka som aldrig för.")
-                    print("Du har fått en stöt och du förlorar 1 hälsopoäng!")
+                    print('''
+                        Du sträcker ut din hand redo att öppna dörren framför dig. När din 
+                        hud kommer i kontakt med dörrhantaget börjar du SssKKaKka som aldrig för.
+                        
+                        Du har fått en stöt och du förlorar 1 hälsopoäng!
+                
+                        Efter en stund av skakande reser du dig, går igenom dörren vars handtag
+                        inte längre är strömförande, och är redo för ditt nästa val
+
+                    ''')
                     self.hp -= 1  
-                    print()
-                    print("Efter en stund av skakande reser du dig, går igenom dörren vars handtag")
-                    print("inte längre är strömförande, och är redo för ditt nästa val")                    ##################
-                    
 
                     
             # -------------------------------------- monster -------------------------------------- #
-            if dörrens_innehåll in {7,8,9}:
+            if dörrens_innehåll == 3:
             
                 if self.lvl < 5:
                     m_styrka = randint(1, 15)
                     m_liv = randint(1, 15)
                     
-                elif self.stark in {5, 6, 7, 8, 9}:
+                elif self.styrka in {5, 6, 7, 8, 9}:
                     m_styrka = randint(10, 20)
                     m_liv = randint(10, 20)
                     
@@ -333,104 +344,82 @@ class Player():
                     m_liv = randint(25, 35)
                 
                 def m_meny():
-                    print("")
-                    print("Vad vill du göra?")
-                    print("")
-                    print("Slå till                   -> a")
-                    print("Spring!                    -> d")
-                    print("Visa styrka och hälsopoäng -> q")
-
-                def moonsret():
-                    monstrets_namn = randint(1, 3)
-
-                    if monstrets_namn == 1:
-                        print('''
+                    print('''
+                        Vad vill du göra?
+                    
+                        - Slå till                    -> a
+                        - Spring!                     -> d
+                        - Visa styrka och hälsopoäng  -> q
                         
-                        Du träffade på monstret Lars Olaf I köket. 
-                        Lars Olaf letar efter den senaste tjock-korven. 
-                        Lars Olaf har letat en lång tid och har inte hittat den. 
-                        Lars Olaf är sur
-                        Lars Olaf märker att du tittar på honom och blir rosenrasande på dig
-                        Lars Olaf attackerar
-
                         ''')
-                        
-                    if monstrets_namn == 2:
-                        print('''
-                        
-                        Monstert Sten Classe flyter på en uppblåsbar svan och dricker piña colada i slottets privata pool
-                        Du tycker det ser ut som en bra chans att ta en paus från ditt äventyr
-                        Du smyger dig fram för att inte störa Sten Classe
-                        Sten Classe hör dig inte
-                        Du sträcker dig efter bordet där det står piña coladas
-                        Glasen klirrar till
-                        Sten Classe vänder sig om och ser dig 
-                        Sten Classe tycker att du har stört honom och han blir arg
-                        Sten Classe attackerar
 
-                        ''')
+                
         
                 while True:
-                    moonsret()
+                    print("Du står nu öga mot öga med ett monster")
                     m_meny()
-                    val = input("-> ")
+                    val = input("Ditt val -> ")
                     
                     if val == "a":
                         strid = randint(1, 3)
                         
                         if strid in (1, 2):
                             print()
-                            print("träffade monstret")
-                            m_liv -= self.stark
+                            print("Du träffade monstret")
+                            m_liv -= self.styrka   # VARFÖR? MAN TRÄFFAR VÄLL INTE SAMMA MONSEER IGEN....
                           
                         else:
                             print()
-                            print("missade monstret")
+                            print("Monstret missade dig")
                         
                     if val == "q":
                         print("")
-                        print(f"Du har {self.stark} i styrka och {self.hp} i liv")
+                        print(f"Du har {self.styrka} i styrka och {self.hp} i liv")
                         print(f"Monstret har {m_styrka} i styka och {m_liv} i liv")
                         print("")
                         continue
 
                     if val == "d":
                         spring = randint(1, 3)
-                        if self.stark < m_styrka:
+                        if self.styrka < m_styrka:
                             if spring == 1:
                                 print()
                                 print("Du sprang iväg")
-                                print("Du är nu redo för ditt nästa val")
+                                redo_för_val() 
                                 break
                         
                             else:
                                 print()
                                 print("Du misslyckades med att springa iväg")
+                            
 
-                        if self.stark > m_styrka:
+                        if self.styrka > m_styrka:
                             if spring in {1, 2}:
-                                
                                 print("Du sprang iväg")
-                                print("Du ä nu redo för ditt nästa val.")
+                                redo_för_val()      
                                 break
 
                             else:
                                 print("Du misslyckade med att springa iväg")
 
-                    if m_liv <= 0:
+                    if m_liv <= 0:                                                 ##### varför? VAD GÖR DEN?
                         print("Monstret blev ledset och sprang iväg")
                         print("Du lvlade upp!")
                         self.lvl += 1
-                        print("Du är nu redo för ditt nästa val.")   #########
+                        redo_för_val() 
                         break
 
                     m_slag_resultat = randint(1, 3)
                     if m_slag_resultat in {1, 2}:
-                        print("monstret missade")
+                        print("Du missade monstret")
+                        redo_för_val() 
+                        break
 
                     elif m_slag_resultat == 3:
                         print("Monstret skadade dig")
                         self.hp -= m_styrka
+                        redo_för_val() 
+                        break
                 
                     if self.hp <= 0:
                         break
@@ -440,7 +429,7 @@ class Player():
             
                    
     def dö(self):
-        print("Du dog och förlorade alla dina hälsopoäng")
+        print("Du dog då du förlorade alla dina hälsopoäng")
         print(f"Du kom till lvl {self.lvl}")
                 
             
@@ -457,13 +446,14 @@ class Player():
 
                 
 def meny():
-    print("")
-    print("")
-    print("Vad vill du göra?")
-    print("Gå frammåt i slottet -> w")
-    print("Kolla stats          -> q")
-    print("kolla inventory      -> e")
-    print("")
+    print('''
+    Vad vill du göra?
+        
+    - Gå frammåt i slottet -> w
+    - Kolla stats          -> q
+    - kolla inventory      -> e
+
+    ''')
 
 #huvudprogram
 random_dude = Player()
@@ -475,7 +465,7 @@ while True:
 
     if val == "e":
         random_dude.visa_inventory()
-        input("för att stänga, klicka på enter")
+        input("för att stänga, klicka på enter") 
 
     if val == "q":
         random_dude.stats()
